f5f1e6787dad863873f108e5387335bb62b91aa3
amshuvo2002 <amshuvo.mail1425@gmail.com>
2025-10-10 02:45:16 +0600

    Some commit

--- src/Pages/AppDetails/AppDetails.jsx
+++ src/Pages/AppDetails/AppDetails.jsx
@@ -1,51 +1,115 @@
-import { useLoaderData, useParams } from "react-router";
+import { Link, useLoaderData, useParams } from "react-router";
 import Star from '../../assets/icon-ratings.png'
 import Download from '../../assets/icon-downloads.png'
-import Like from '../../assets/icon-ratings.png'
+import Like from '../../assets/icon-review.png'
+import AppError from '../../assets/App-Error.png'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 function AppDetails() {
-  const data = useLoaderData();   // JSON data
+  const data = useLoaderData();
   const { id } = useParams();
-
   const app = data.find(app => app.id === parseInt(id));
 
   if (!app) {
-    return <h2 className="text-center text-red-500 mt-10">App not found 😢</h2>;
+    return (
+      <div className=" bg-gray-100 p-10">
+        <div>
+          <img className="w-[400px] mx-auto" src={AppError} alt="" />
+        </div>
+        <div className="text-center mt-5">
+          <h1 className="text-4xl font-bold">OPPS!! APP NOT FOUND</h1>
+          <p className="text-[#627382]">
+            The App you are requesting is not found on our system. Please try another app.
+          </p>
+          <Link to={'/Apps'}>
+            <button className="btn btn-primary bg-gradient-to-r from-[#632EE3] to-[#9F62F2] mt-2">
+              Go Back!
+            </button>
+          </Link>
+        </div>
+      </div>
+    );
   }
+
+
+  const sortedRatings = [...app.ratings].sort((a, b) => {
+    const numA = parseInt(a.name);
+    const numB = parseInt(b.name);
+    return numB - numA; 
+  });
 
   return (
     <div className="bg-gray-100">
-        <div className="p-20">
-            <div className="w-[300px] h-[300px] bg-white flex justify-center items-center"><img className="w-[220px] h-[220px]" src={app.image} alt="" /></div>
-            <div>
-                <div>
-                    <h1>{app.title}</h1>
-                    <p>Developed by <span>{app.companyName}</span></p>
-                </div>
-                <div>
-                    <div>
-                        <img src={Download} alt="" />
-                        <p>Downloads</p>
-                        <h1>{app.downloads}</h1>
-                        </div>
-                    <div>
-                        <img src={Star} alt="" />
-                        <p>Average Ratings</p>
-                        <h1>{app.ratingAvg}</h1>
-                    </div>
-                    <div>
-                        <img src={Like} alt="" />
-                        <p>Total Reviews</p>
-                        <h1>{app.reviews}</h1>
-                    </div>
-                </div>
-                <div></div>
-            </div>
-        </div>
     
+      <div className="md:p-20 p-10 md:flex items-center gap-10">
+        <div className="w-[300px] h-[300px] bg-white flex justify-center items-center">
+          <img className="w-[220px] h-[220px]" src={app.image} alt="" />
+        </div>
+
+        <div>
+          <div className="mb-5">
+            <h1 className="text-4xl font-bold">{app.title}</h1>
+            <p>
+              Developed by{" "}
+              <span className="bg-gradient-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">
+                {app.companyName}
+              </span>
+            </p>
+          </div>
+
+          <div className="flex gap-10 mb-5">
+            <div>
+              <img src={Download} alt="" />
+              <p>Downloads</p>
+              <h1 className="text-4xl font-bold">{app.downloads}</h1>
+            </div>
+            <div>
+              <img src={Star} alt="" />
+              <p>Average Ratings</p>
+              <h1 className="text-4xl font-bold">{app.ratingAvg}</h1>
+            </div>
+            <div>
+              <img src={Like} alt="" />
+              <p>Total Reviews</p>
+              <h1 className="text-4xl font-bold">{app.reviews}</h1>
+            </div>
+          </div>
+
+          <button className="btn btn-accent mt-5 md:mt-0 text-white bg-[#00D390]">
+            Install Now - {app.size} MB
+          </button>
+        </div>
+      </div>
+
+      
+      <div className=" md:p-10 p-5 rounded-2xl shadow-md md:mx-20 mb-10">
+        <h2 className="text-2xl font-semibold mb-5">
+          Ratings
+        </h2>
+
+        <ResponsiveContainer width="100%" height={300}>
+          <BarChart
+            layout="vertical"
+            data={sortedRatings}
+            margin={{ top: 5, right: 10, left: 10, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis type="number" />
+            <YAxis dataKey="name" type="category" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="count" fill="#FF8811" radius={[0, 6, 6, 0]} />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+
+     
+      <div className="md:px-20 px-10 pb-5">
+        <h1 className="text-2xl font-bold pb-4">Description :</h1>
+        <p>{app.description}</p>
+      </div>
     </div>
   );
 }
 
 export default AppDetails;
-
--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -25,32 +25,14 @@ const Installation = () => {
                         <div className="dropdown dropdown-start">
                             <div tabIndex={0} role="button" className="btn m-1">Sort By Size ⬇️</div>
                             <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                                <li><a>Item 1</a></li>
-                                <li><a>Item 2</a></li>
+                                <li><a>High-Low</a></li>
+                                <li><a>Low-High </a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='md:px-20 px-5'>
-               
-                   <div className='md:flex justify-between items-center shadow-xl gap-5 bg-white p-5 rounded-sm mb-10 '>
-                        <div className='md:flex gap-5'>
-                            <div>  <img className='w-[100px]' src={App} alt="" /></div>
-                            <div className='' >
-                                <h1 className='text-xl font-bold'>Forest: Focus for Productivity</h1>
-                                <div className='flex justify-around mt-5'>
-                                    <div className='flex justify-between gap-2 p-2 rounded-sm text-[#00D390] bg-[#F1F5E8]'><img className='w-[20px]' src={Download} alt="" />{}</div>
-                                    <div className='flex justify-between gap-2 p-2 rounded-sm text-[#FF8811] bg-[#FFF0E1]'><img className='w-[20px]' src={Star} alt="" />{}</div>
-                                    <div className='flex justify-center items-center'><p className='font-semibold text-gray-400'>450 MB</p></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <button className="btn btn-accent mt-5 md:mt-0 bg-[#00D390]">Uninstall</button>
-                        </div>
-                    </div>
-
+            <div className='md:px-20 px-5 '>
 
                     <div className='md:flex justify-between items-center shadow-xl gap-5 bg-white p-5 rounded-sm mb-10 '>
                         <div className='md:flex gap-5'>
@@ -65,7 +47,7 @@ const Installation = () => {
                             </div>
                         </div>
                         <div>
-                           <button className="btn btn-accent mt-5 md:mt-0 bg-[#00D390]">Uninstall</button>
+                           <button className="btn btn-accent mt-5 text-white md:mt-0 bg-[#00D390]">Uninstall</button>
                         </div>
                     </div>
             </div>
